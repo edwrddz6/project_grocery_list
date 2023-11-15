1d9bc2e623a815451df6c659b604d31a70058e33
Edward Diaz <diazedward24@gmail.com>
2023-11-15 14:15:26 -0800

    Add variables, add function with forEach

---
+++ script.js
@@ -0,0 +1,32 @@
+const groceryItems = [
+    { name: 'Apple', category: 'Fruits', price: 1.0, quantity: 6 },
+    { name: 'Banana', category: 'Fruits', price: 0.5, quantity: 10 },
+    { name: 'Carrot', category: 'Vegetables', price: 0.8, quantity: 8 },
+    { name: 'Milk', category: 'Dairy', price: 2.0, quantity: 2 },
+    { name: 'Eggs', category: 'Dairy', price: 1.5, quantity: 12 }
+];
+
+const groceryList = document.querySelector('#groceryList');
+
+const totalCostElement = document.querySelector('#totalCost');
+
+const totalQuantityElement = document.querySelector('#totalQuantity');
+
+
+displayGroceryItems = () => {
+    groceryList.innerHTML = ';'
+
+    let totalCost = 0;
+    let totalQuantity = 0;
+
+    groceryItems.forEach(item => {
+        const itemDiv = document.createElement("div");
+        itemDiv.textContent = `<b>${item.name}</b> (${item.category}): $${item.price} - ${item.quantity}`;
+        groceryList.appendChild(itemDiv);
+
+        totalCost += item.price * item.quantity;
+        totalQuantity += item.quantity;
+    })
+
+}
+
