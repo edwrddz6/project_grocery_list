ce425723da6b3fab4d9ef00bffb04aea654d357b
Edward Diaz <diazedward24@gmail.com>
2023-11-15 19:17:48 -0800

    Add isolate function

--- script.js
+++ script.js
@@ -14,19 +14,34 @@ const totalQuantityElement = document.querySelector('#totalQuantity');
 
 
 displayGroceryItems = () => {
-    groceryList.innerHTML = ';'
+    groceryList.innerHTML = '';
 
     let totalCost = 0;
     let totalQuantity = 0;
 
     groceryItems.forEach(item => {
         const itemDiv = document.createElement("div");
-        itemDiv.textContent = `<b>${item.name}</b> (${item.category}): $${item.price} - ${item.quantity}`;
+        itemDiv.innerHTML = `<b>${item.name}</b> (${item.category}): $${item.price} - ${item.quantity}`;
         groceryList.appendChild(itemDiv);
 
         totalCost += item.price * item.quantity;
         totalQuantity += item.quantity;
-    })
+    });
 
+    totalCostElement.textContent = `Total Cost: $${totalCost.toFixed(2)}`;
+    totalQuantityElement.textContent = `Total Quantity: ${totalQuantity}`;
 }
 
+isolateCategory = (category) => {
+    groceryItems.innerHTML = '';
+
+    const filteredItems = groceryItems.filter(item => {
+        return item.category === category;
+    });
+
+    filteredItems.forEach(item => {
+        const itemDiv = document.createElement("div");
+        itemDiv.innerHTML = `<b>${item.name}</b> (${item.category}): $${item.price} - ${item.quantity}`;
+        filteredItems.appendChild(itemDiv);
+    })
+}
